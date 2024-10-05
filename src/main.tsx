9f3ffb02348a1366f146d5cbc0b2e61786fc0069
uzair166 <uzair.i@hotmail.co.uk>
2024-10-05 03:53:56 +0100

    feat: add analytics tracking with vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { inject } from "@vercel/analytics";
+
+inject(); // Inject the Vercel Analytics tracking script
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
